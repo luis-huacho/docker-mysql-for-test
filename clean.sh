5ea87883e22a32ac7947014e75bbc3cef0d9e453
Luis Huacho <l.huacho@gmail.com>
2024-07-13 12:43:48 -0500

    Optimizado para MySQL 5.7

---
+++ clean.sh
@@ -0,0 +1,2 @@
+docker compose --env-file=config-env -f docker-compose.yml down
+sudo rm -rf logs mysql-data
---
+++ logs.sh
@@ -0,0 +1,6 @@
+mkdir logs
+touch logs/error.log
+touch logs/general.log
+touch logs/slow.log
+
+sudo chmod -R 777 logs
